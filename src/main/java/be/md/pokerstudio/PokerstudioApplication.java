5a9f10bd93ad80df7d6e184b59e197d4dcd7d790
Mark Dechamps <mark.dechamps@gmail.com>
2022-05-15 19:49:04 +0200

    first commit

---
+++ src/main/java/be/md/pokerstudio/PokerstudioApplication.java
@@ -0,0 +1,13 @@
+package be.md.pokerstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerstudioApplication.class, args);
+	}
+
+}
